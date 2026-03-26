fa5867be6a7bd2c9a7a28bc5412e678f7d41ba3a
Buhle M <mthembbu@gmail.com>
2026-03-26 13:24:53 +0200

    Update App.java

--- src/main/java/com/tstcore/jpa_inheritance_hierarchy/App.java
+++ src/main/java/com/tstcore/jpa_inheritance_hierarchy/App.java
@@ -1,5 +1,9 @@
 package com.tstcore.jpa_inheritance_hierarchy;
 
+/** TABLE_PER_CLASS  states that a table per concrete entity class is created.
+ *
+ */
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
