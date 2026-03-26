fb8969ba3275be0a7aca40f9d4cbcde6189fa960
Buhle M <mthembbu@gmail.com>
2026-03-26 13:25:48 +0200

    Update App.java

--- src/main/java/com/tstcore/jpa_inheritance_hierarchy/App.java
+++ src/main/java/com/tstcore/jpa_inheritance_hierarchy/App.java
@@ -1,5 +1,10 @@
 package com.tstcore.jpa_inheritance_hierarchy;
 
+/** SINGLE_Table is a default strategy for inheritance.
+ * It maps all classes in the hierarchy to a single table in the database.
+ *
+ */
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
